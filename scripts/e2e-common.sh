9b4813060008f52f4427ae5cb9fbeea46b47e321
Dawid Ciężarkiewicz <dpc@dpc.pw>
2017-07-26 00:32:28 -0700

    TEst

--- scripts/e2e-common.sh
+++ scripts/e2e-common.sh
@@ -1,8 +1,8 @@
 test_data_path=/tmp/rdedup-e2e-test.data
-chunking_list=`cargo run --release -- init --help | grep chunking | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ','`
-encryptiton_list=`cargo run --release -- init --help | grep encryption | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ','`
-compression_list=`cargo run --release -- init --help | grep compression | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ','`
-hashing_list=`cargo run --release -- init --help | grep hashing | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ','`
+chunking_list=$(cargo run --release -- init --help | grep chunking | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ',')
+encryptiton_list=$(cargo run --release -- init --help | grep encryption | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ',')
+compression_list=$(cargo run --release -- init --help | grep compression | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ',')
+hashing_list=$(cargo run --release -- init --help | grep hashing | sed 's/.*\[values: \(.*\)\].*/\1/' | tr -d ',')
 
 export RDEDUP_DIR=/tmp/rdedup-e2e-test.repo
 export RDEDUP_PASSPHRASE=testing
